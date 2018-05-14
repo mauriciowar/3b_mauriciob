0ee73a3fb94f0a8a4aff30d4389b66b32ffbaf02
01299399274 <01299399274@JIPA-LABIN03-07.ifro.local>
2018-05-14 13:21:26 -0400

    BD

--- Grupo3/src/br/edu/ifro/FXMLDocumentController.java
+++ Grupo3/src/br/edu/ifro/FXMLDocumentController.java
@@ -5,14 +5,19 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 /**
@@ -26,6 +31,8 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML 
     private Button btnSoma;
+    @FXML
+    private Label label;
     
     @FXML
     private void soma(ActionEvent event) {
@@ -41,5 +48,25 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void AbrirCadastroAluno(ActionEvent event) {
+    
+         try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Cliente.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+    stage.setTitle("Cadastrar Cliente");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+    @FXML
+    private void fechar(ActionEvent event) {
+    }
     
 }
